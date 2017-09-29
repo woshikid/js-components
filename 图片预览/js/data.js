75f03b3b0f173859c170e7b46f9b9838983c7eed
woshikid <name.kid@gmail.com>
2017-09-29 11:21:55 +0800

    增加图片预览控件

---
+++ 图片预览/js/data.js
@@ -0,0 +1,22 @@
+var imgData = [
+	{
+		src: 'images/a1.jpg',
+		title: 'a1.jpg',
+		name: '火影忍着1'
+	},
+	{
+		src: 'images/a2.jpg',
+		title: 'a2.jpg',
+		name: '火影忍着3'
+	},
+	{
+		src: 'images/a3.jpg',
+		title: 'a3.jpg',
+		name: '火影忍着3'
+	},
+	{
+		src: 'images/a4.jpg',
+		title: 'a4.jpg',
+		name: '火影忍着4'
+	}
+];
